48059ecddea554b2ee6e63ff19d832079efb37b9
jfigueiredoo <jfigueiredo409@gmail.com>
2024-10-20 13:41:40 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,41 +1,35 @@
 import ui from "./ui.js"
 import api from "./api.js"
 
-async function manipularSubmissaoFormulario(event) {
-    event.preventDefault()
-    const id = document.getElementById("pensamento-id").value
-    const conteudo = document.getElementById("pensamento-conteudo").value
-    const autoria = document.getElementById("pensamento-autoria").value
-
-    try {
-        await api.salvarPensamento({ conteudo, autoria })
-        ui.renderizarPensamentos()
-    }
-    catch {
-        alert("Erro ao salvar pensamento")
-    }
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-    ui.renderizarPensamentos()
+  ui.renderizarPensamentos()
 
-    const formularioPensamento = document.getElementById("pensamento-form")
+  const formularioPensamento = document.getElementById("pensamento-form")
+  const botaoCancelar = document.getElementById("botao-cancelar")
 
-
-    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+  formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+  botaoCancelar.addEventListener("click", manipularCancelamento)
 })
 
 async function manipularSubmissaoFormulario(event) {
-    event.preventDefault()
-    const id = document.getElementById("pensamento-id").value
-    const conteudo = document.getElementById("pensamento-conteudo").value
-    const autoria = document.getElementById("pensamento-autoria").value
+  event.preventDefault()
+  const id = document.getElementById("pensamento-id").value
+  const conteudo = document.getElementById("pensamento-conteudo").value
+  const autoria = document.getElementById("pensamento-autoria").value
 
-    try {
-        await api.salvarPensamento({ conteudo, autoria })
-        ui.renderizarPensamentos()
+  try { 
+    if(id) {
+      await api.editarPensamento({ id, conteudo, autoria })
+    } else {
+      await api.salvarPensamento({ conteudo, autoria })
     }
-    catch {
-        alert("Erro ao salvar pensamento")
-    }
+    ui.renderizarPensamentos()
+  }
+  catch {
+    alert("Erro ao salvar pensamento")
+  }
+}
+
+function manipularCancelamento() {
+  ui.limparFormulario()
 }
